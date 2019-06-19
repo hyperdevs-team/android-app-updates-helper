695c8f6a8283e26d59ece54b739c1c39e32e3563
Adrián García <adrian.garcia@bq.com>
2019-06-19 17:05:23 +0200

    Initial commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,46 @@
+# Contributing to AppUpdatesHelper
+
+We love your input! We want to make contributing to this project as easy as possible, whether it's:
+
+- Reporting a bug
+- Submitting a fix
+- Proposing new features
+- Discussing the current state of the code
+
+## We Develop with GitHub
+We use GitHub to host code, to track issues and feature requests, as well as to accept pull requests.
+
+## Report bugs using Github's [issues](https://github.com/bq/AppUpdatesHelper/issues)
+We use GitHub issues to track public bugs. Report a bug by [opening a new issue](https://github.com/bq/AppUpdatesHelper/issues/new)!
+
+## Code Changes Happen Through Pull Requests
+Pull requests are the best way to propose changes to the codebase. We actively welcome your pull requests:
+
+1. Fork the repo and create your branch from `master`.
+2. If you've added code that should be tested, add tests.
+3. If you've changed APIs, update the documentation.
+4. Make sure your code lints and passes the Checkstyle rules.
+5. Issue that pull request!
+
+## Any contributions you make will be under the Apache 2.0 Software License
+When you submit code changes, your submissions are understood to be under the same [Apache License 2.0](http://choosealicense.com/licenses/apache-2.0/) that covers the project. 
+Feel free to contact the maintainers if that's a concern.
+
+## Write bug reports with detail, background, and sample code
+**Great Bug Reports** tend to have:
+
+- A quick summary and/or background
+- Steps to reproduce
+  - Be specific!
+  - Give sample code if you can in order to reproduce the issue
+  - Add the stack traces you find in case the library makes your app crash
+- What you expected would happen
+- What actually happens
+- Notes (possibly including why you think this might be happening, or stuff you tried that didn't work)
+
+## Use a Consistent Coding Style
+We provide a [code-style](code-style.xml) and [checkstyle](checkstyle.xml) configuration files so you can ensure that all code is properly formatted according to our guidelines.
+Please be sure to use these files to format your files when submitting pull requests.
+
+## References
+This document was adapted from the contribution guidelines proposed by [Brian A. Danielak](https://gist.github.com/briandk/3d2e8b3ec8daf5a27a62) which are itself based on the open-source contribution guidelines for [Facebook's Draft](https://github.com/facebook/draft-js/blob/a9316a723f9e918afde44dea68b5f9f39b7d9b00/CONTRIBUTING.md)
---
+++ app/src/main/kotlin/com/bq/appupdateshelper/fake/FakeUpdateActivity.kt
@@ -0,0 +1,204 @@
+/*
+ * Copyright (C) 2019 BQ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bq.appupdateshelper.fake
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.bq.appupdateshelper.AppUpdateInfoResult
+import com.bq.appupdateshelper.AppUpdateInstallState.Status.*
+import com.bq.appupdateshelper.FakeAppUpdatesHelper
+import com.bq.appupdateshelper.R
+import com.bq.appupdateshelper.misc.showToast
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * Activity that illustrates the use of the [FakeAppUpdatesHelper] class of the lib to simulate
+ * app updates in test environments.
+ *
+ * In this class we'll simulate a flexible update scenario. In order to understand how flexible
+ * updates work, please review [com.bq.appupdateshelper.flexible.FlexibleUpdateActivity].
+ */
+class FakeUpdateActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "FakeUpdateActivity"
+        fun newIntent(context: Context): Intent {
+            return Intent(context, FakeUpdateActivity::class.java)
+        }
+    }
+
+    private lateinit var fakeAppUpdatesHelper: FakeAppUpdatesHelper
+
+    private val startUpdateButton: Button
+        get() = findViewById(R.id.start_update_button)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fake_update)
+
+        setTitle(R.string.activity_fake_update_title)
+
+        fakeAppUpdatesHelper = FakeAppUpdatesHelper(this)
+
+        fakeAppUpdatesHelper.startListening { installState ->
+
+            Log.d(TAG, "Update install state: $installState")
+
+            when (installState.status) {
+                UNKNOWN -> {
+                    showToast("Unknown install state")
+                }
+                DENIED -> {
+                    showToast("The user denied the flexible update!")
+                    finish()
+                }
+                REQUIRES_UI_INTENT -> {
+                    showToast("The update needs an UI intent!")
+                }
+                UPDATE_ACCEPTED -> {
+                    showToast("The user accepted the update!")
+                }
+                PENDING -> {
+                    showToast("Waiting for update to start!")
+                }
+                DOWNLOADING -> {
+                    showToast("The update is downloading!")
+                }
+                DOWNLOADED -> {
+                    showToast("The update has been downloaded!")
+
+                    Snackbar.make(findViewById(android.R.id.content), "Install the update?", Snackbar.LENGTH_INDEFINITE)
+                        .apply {
+                            setAction("Install") {
+                                fakeAppUpdatesHelper.completeUpdate()
+
+                                // Fake installation process
+                                fakeAppUpdatesHelper.completeFakeUpdate()
+                            }
+                        }
+                        .show()
+                }
+                INSTALLING -> {
+                    showToast("The update is being installed!")
+                }
+                INSTALLED -> {
+                    showToast("The update has been installed!")
+                }
+                FAILED -> {
+                    showToast("The update failed! Reason: ${installState.errorCode}")
+
+                    Snackbar.make(findViewById(android.R.id.content), "Retry?", Snackbar.LENGTH_LONG)
+                        .apply {
+                            setAction("Retry") {
+                                fakeAppUpdatesHelper.startImmediateUpdate(this@FakeUpdateActivity)
+                            }
+                        }
+                        .show()
+                }
+                CANCELED -> {
+                    showToast("The user canceled the flexible update!")
+                }
+            }
+        }
+
+        startUpdateButton.setOnClickListener {
+            // We're going to set-up the fake in order to simulate an available update
+            fakeAppUpdatesHelper.configAvailableUpdate()
+
+            fakeAppUpdatesHelper.getAppUpdateInfo { appUpdateInfoResult ->
+                Log.d(TAG, "App update info: $appUpdateInfoResult")
+
+                if (appUpdateInfoResult.isSuccessful) {
+                    when (appUpdateInfoResult.updateAvailability!!) {
+                        AppUpdateInfoResult.Availability.UNKNOWN -> {
+                            showToast("The state of the update is unknown!")
+                        }
+                        AppUpdateInfoResult.Availability.UPDATE_NOT_AVAILABLE -> {
+                            showToast("No update available!")
+                        }
+                        AppUpdateInfoResult.Availability.UPDATE_AVAILABLE -> {
+                            showToast("Update available!")
+                            if (appUpdateInfoResult.canInstallFlexibleUpdate()) {
+                                // Start the update flow
+                                showToast("Can install flexible update!")
+                                fakeAppUpdatesHelper.startFlexibleUpdate(this)
+
+                                // Start faking the update flow
+                                fakeAppUpdatesHelper.startFakeInstallFlow()
+                            } else {
+                                showToast("Can not install flexible update!")
+                            }
+                        }
+                        AppUpdateInfoResult.Availability.UPDATE_IN_PROGRESS -> {
+                            showToast("Update in progress!")
+                        }
+                        AppUpdateInfoResult.Availability.UPDATE_DOWNLOADED -> {
+                            showToast("Update downloaded!")
+                            fakeAppUpdatesHelper.completeUpdate()
+
+                            // Start faking installation process
+                            fakeAppUpdatesHelper.completeFakeUpdate()
+                        }
+                    }
+                } else {
+                    showToast("The update info could not be retrieved, " +
+                              "cause: ${appUpdateInfoResult.exception!!.message}")
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // We bind this but the fake will not launch an external activity, so it won't effect
+        // the flow
+        fakeAppUpdatesHelper.onUpdateStatusResult(requestCode, resultCode)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        fakeAppUpdatesHelper.stopListening()
+    }
+}
+
+private fun FakeAppUpdatesHelper.configAvailableUpdate() {
+    setUpdateAvailable(12345)
+}
+
+private fun FakeAppUpdatesHelper.startFakeInstallFlow() {
+    val handler = Handler()
+
+    with(handler) {
+        postDelayed({ userAcceptsUpdate() }, 0)
+        postDelayed({ downloadStarts() }, 1000)
+        postDelayed({ downloadCompletes() }, 4000)
+    }
+}
+
+private fun FakeAppUpdatesHelper.completeFakeUpdate() {
+    val handler = Handler()
+
+    with(handler) {
+        postDelayed({ installCompletes() }, 4000)
+    }
+}
